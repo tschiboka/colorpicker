042d592bd1e24dc77501ced452a43433e480b4bd
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-02-06 02:01:57 +0000

    get hue and alpha slider bases

--- src/Components/ColorPicker/ColorPicker.js
+++ src/Components/ColorPicker/ColorPicker.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "./ColorPicker.scss";
 import btnBg from "./images/btn_sm_square.png";
+import transparentCheckerdBg from "./images/transparent_checkered_bg.png";
 
 export default class ColorPicker extends Component {
     constructor(props) {
@@ -16,22 +17,46 @@ export default class ColorPicker extends Component {
                     className="ColorPicker"
                     style={{ left: (this.props.X || 0) + "px", top: (this.props.Y || 0) + "px" }}
                     onClick={e => e.stopPropagation()}
-                    onBlur={() => this.props.close()}
+                    //onBlur={() => this.props.close()}
+                    onKeyDown={e => { if (e.keyCode === 27) this.props.close(); }}
                     tabIndex={1}
                     ref={component => { if (ReactDOM.findDOMNode(component)) ReactDOM.findDOMNode(component).focus() }}
                 >
                     <div className="ColorPicker__header">
                         <button
-                            className="close-btn" onClick={() => this.props.close()}
+                            className="close-btn"
+                            onClick={() => this.props.close()}
                             style={{ backgroundImage: `url(${btnBg})` }}
+                            title="close [Esc]"
                         >&times;</button>
                     </div>
 
-                    <div className="ColorPicker__palette"></div>
+                    <div className="ColorPicker__body">
+                        <div className="ColorPicker__upper-box">
+                            <div className="ColorPicker__palette"></div>
 
-                    <div className="ColorPicker__spectrum"></div>
+                            <div className="ColorPicker__text-inputs"></div>
+                        </div>
 
-                    <div className="ColorPicker__alpha"></div>
+                        <div className="ColorPicker__lower-box">
+                            <div className="ColorPicker__hue-slider">
+                                <div className="ColorPicker__hue"></div>
+                            </div>
+
+                            <div className="ColorPicker__alpha-box">
+                                <div className="ColorPicker__alpha-slider">
+                                    <div className="ColorPicker__alpha-bg" style={{ backgroundImage: `url(${transparentCheckerdBg})` }}>
+
+                                        <div className="ColorPicker__alpha"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="ColorPicker__ok-btn-box">
+                                <button style={{ backgroundImage: `url(${btnBg})` }}>OK</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>)
                 : null
         );
