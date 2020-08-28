7bbe48fa1ed30c07118c2781881c6236e8132d3c
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-08-29 00:28:57 +0100

    add zig zag animation

--- src/Components/Animation/Animation.js
+++ src/Components/Animation/Animation.js
@@ -5,20 +5,28 @@ import "./Animation.scss";
 
 export default function Animation(props) {
     function renderLetters(letters) {
-        return [...letters].map((letter, index) => <span key={`letter_${index}`}>{letter}</span>);
+        return [...letters].map((letter, index) => (
+            <span key={`letter_${index}`}>
+                {letter}
+            </span>)
+        );
     }
 
 
 
     return (
         <div className="Animation">
-            <div>{renderLetters("Gradient")}</div>
+            <div className="Animation__zig-zag"></div>
 
-            <div><span>&</span></div>
+            <div className="Animation__text">{renderLetters("Gradient")}</div>
 
-            <div>{renderLetters("Pattern")}</div>
+            <div className="Animation__text"><span>&</span></div>
 
-            <div>{renderLetters("Generator")}</div>
+            <div className="Animation__text">{renderLetters("Pattern")}</div>
+
+            <div className="Animation__text">{renderLetters("Generator")}</div>
+
+            <div className="Animation__zig-zag"></div>
         </div>
     );
 }
